59d6f1b07106cae14900929d68602a01ec92c2e3
Andreas Ohlander <andreas.ohlander@gmail.com>
2024-12-04 20:51:08 +0100

    #9 - Added most components and init method still needs table setup

--- src/View/GUI.java
+++ src/View/GUI.java
@@ -1,12 +1,122 @@
 package View;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GUI {
     JFrame frame;
-    JPanel panel;
+
+    JPanel mainPanel;
+
+    JPanel topPanel;
     JLabel searchLabel;
     JTextField searchField;
+    JPanel radioButtonPanel;
+    ButtonGroup buttonGroup;
+    JRadioButton radioButtonName;
+    JRadioButton radioButtonID;
+    JLabel filterLabel;
+    JComboBox<String> filterComboBox;
 
+    JPanel centerPanel;
+    JPanel searchResultPanel;
+    JScrollPane searchResultScrollPane;
+
+    JPanel showDetailsMainPanel;
+    JPanel showDetailsTopPanel;
+    JButton showDetailsButton;
+    JPanel showDetailsCenterPanel;
+    JTextField showDetailsNameTextField;
+    JTextField showDetailsEmploymentPercentageTextField;
+    JTextField showDetailsPositionTextField;
+    JTextField showDetailsEmailTextField;
+    JTextField showDetailsSalaryTextField;
+    JTextField showDetailsPhoneTextField;
+
+
+
+
+    public GUI() {
+        frame = new JFrame();
+        mainPanel = new JPanel();
+        topPanel = new JPanel();
+        searchLabel = new JLabel("Search");
+        searchField = new JTextField();
+        radioButtonPanel = new JPanel();
+        buttonGroup  = new ButtonGroup();
+        radioButtonName = new JRadioButton("Name");
+        radioButtonID = new JRadioButton("ID");
+        filterLabel = new JLabel("Filter");
+        filterComboBox = new JComboBox<>(new String[]{"Developer", "Manager", "HR"});
+        centerPanel = new JPanel();
+        searchResultPanel = new JPanel();
+        searchResultScrollPane = new JScrollPane(); // TODO ADD TABLE MODEL HERE
+        showDetailsMainPanel = new JPanel();
+        showDetailsTopPanel = new JPanel();
+        showDetailsButton = new JButton("Show Details");
+        showDetailsCenterPanel = new JPanel();
+        showDetailsNameTextField = new JTextField("Fake Name");
+        showDetailsEmploymentPercentageTextField = new JTextField("Fake Employment");
+        showDetailsPositionTextField = new JTextField("Fake Position");
+        showDetailsEmailTextField = new JTextField("Fake Email");
+        showDetailsSalaryTextField = new JTextField("Fake Salary");
+        showDetailsPhoneTextField = new JTextField("Fake Phone");
+    }
+
+    public void init() {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.add(mainPanel);
+
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.add(topPanel, BorderLayout.NORTH);
+        mainPanel.add(centerPanel, BorderLayout.CENTER);
+
+
+        buttonGroup.add(radioButtonName);
+        buttonGroup.add(radioButtonID);
+        radioButtonPanel.setLayout(new GridLayout(2, 1));
+        radioButtonPanel.add(radioButtonName);
+        radioButtonPanel.add(radioButtonID);
+
+        topPanel.setLayout(new GridLayout(1,5));
+        topPanel.add(searchLabel);
+        topPanel.add(searchField);
+        topPanel.add(radioButtonPanel);
+        topPanel.add(filterLabel);
+        topPanel.add(filterComboBox);
+
+        centerPanel.setLayout(new GridLayout(2,1));
+        centerPanel.add(searchResultPanel);
+        centerPanel.add(showDetailsMainPanel);
+
+        searchResultPanel.setLayout(new GridLayout(1,1));
+        searchResultPanel.add(new JButton("Testing size"));
+
+
+        showDetailsMainPanel.setLayout(new BorderLayout());
+        showDetailsMainPanel.add(showDetailsTopPanel, BorderLayout.NORTH);
+        showDetailsTopPanel.setLayout(new GridLayout(1,1));
+        showDetailsTopPanel.add(showDetailsButton, BorderLayout.NORTH);
+
+        showDetailsMainPanel.add(showDetailsCenterPanel);
+        showDetailsCenterPanel.setLayout(new GridLayout(2,3));
+        showDetailsCenterPanel.add(showDetailsNameTextField);
+
+        showDetailsCenterPanel.add(showDetailsEmploymentPercentageTextField);
+        showDetailsCenterPanel.add(showDetailsPositionTextField);
+        showDetailsCenterPanel.add(showDetailsEmailTextField);
+        showDetailsCenterPanel.add(showDetailsSalaryTextField);
+        showDetailsCenterPanel.add(showDetailsPhoneTextField);
+
+
+
+
+
+
+
+    }
 
 }
