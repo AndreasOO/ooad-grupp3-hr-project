a58cb746a7640b2615eba053925af082fb2f85ab
Linn Edvardsson <linn.edvardsson@yh.nackademin.se>
2024-12-05 11:16:25 +0100

    changes

--- src/Database/EmployeeDatabase.java
+++ src/Database/EmployeeDatabase.java
@@ -13,36 +13,21 @@ public class EmployeeDatabase {
 
     }
 
-    public void loadMap(){
+    public void loadMap() {
         employees = Arrays.asList(
-        new Employee(203214, "Andreas Ohlander", 85900, Position.PRODUCTOWNER),
-        new Employee(1000, "Elsa", 20000, Position.DEVELOPER),
-        new Employee(2345, "Karen", 20000, Position.SUBORDINATE),
-        new Employee(1, "Tommy Olsson", 100, Position.CHEF),
-        new Employee(123465, "Linn Edvardsson", 100000, Position.MANAGER),
-        new Employee(539895, "Jesper Lindberg", 76000, Position.DEVELOPER),
-        new Employee(45660, "Josefin Törner", 900000, Position.VD),
-        new Employee(758938, "Ylva Fröjdmark", 33000, Position.SCRUM_MASTER),
-        new Employee(319487, "Simba", 12000, Position.SUBORDINATE)
-);
-        employees.forEach(employee -> {Employees.put(employee.getEmployeeId(), employee);});
+                new Employee(203214, "Andreas Ohlander", 85900, Position.PRODUCTOWNER),
+                new Employee(1000, "Elsa", 20000, Position.DEVELOPER),
+                new Employee(2345, "Karen", 20000, Position.SUBORDINATE),
+                new Employee(1, "Tommy Olsson", 100, Position.CHEF),
+                new Employee(123465, "Linn Edvardsson", 100000, Position.MANAGER),
+                new Employee(539895, "Jesper Lindberg", 76000, Position.DEVELOPER),
+                new Employee(45660, "Josefin Törner", 900000, Position.VD),
+                new Employee(758938, "Ylva Fröjdmark", 33000, Position.SCRUM_MASTER),
+                new Employee(319487, "Simba", 12000, Position.SUBORDINATE)
+        );
+        employees.forEach(employee -> {
+            Employees.put(employee.getEmployeeId(), employee);
+        });
 
-    }
-    public List<Employee> searchByID(long id) {
-        List<Employee> result = new ArrayList<>();
-            if (Employees.containsKey(id)) {
-                result.add(Employees.get(id));
-            }
-            return result;
-        }
-
-    public List<Employee> searchByName(String search) {
-        List<Employee> matchingEmployees = new ArrayList<>();
-        for (Employee employee : Employees.values()) {
-            if (employee.getName().trim().equalsIgnoreCase(search.trim())) {
-                matchingEmployees.add(employee);
-            }
-        }
-        return matchingEmployees;
     }
 }
