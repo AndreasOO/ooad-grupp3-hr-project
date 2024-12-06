e1481919d83588f1c30aef130df8455d62360120
Linn Edvardsson <linn.edvardsson@yh.nackademin.se>
2024-12-06 14:21:16 +0100

    added actionListener for searchfield

--- src/controller/HRController.java
+++ src/controller/HRController.java
@@ -3,14 +3,35 @@ package controller;
 import Model.HRModel;
 import View.HRView;
 
+import javax.swing.*;
+
 public class HRController {
 
     private HRView view;
     private HRModel model;
 
     public HRController(HRModel model) {
+        this.model = model;
         view = new HRView(model);
         view.init();
+        addActionListeners();
+    }
+
+    public void addActionListeners(){
+        view.getSearchField().addActionListener(e->{
+            System.out.println("We made it here!?");
+            String searchTerm = view.getSearchField().getText();
+            if (searchTerm.isEmpty()){
+                JOptionPane.showMessageDialog(null, "Cannot leave text field empty.");
+                return;
+            }
+            if (view.getRadioButtonName().isSelected()){
+                model.setSearchResultByName(searchTerm);
+            } else if (view.getRadioButtonID().isSelected()) {
+                model.setSearchResultById(Long.parseLong(searchTerm));
+            }
+        });
+
     }
 
 
